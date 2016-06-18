e76b471f4c641197439afabf1f4b5153e1a21021
Paulina Friemann <paulina@friemann.com>
2016-06-18 19:07:44 +0200

    parse all

--- ironet/simile.py
+++ ironet/simile.py
@@ -1,16 +1,17 @@
 import os
 import pickle
 from words import Ground, Vehicle
-from words import WordDatabase as wd
+from words import WordDatabase as WD
 
 import utils.query_utils as query_utils
+
+database = WD()
 
 
 def get_inverses(simile):
         inverses = []
         for antonym in simile.ground.antonyms:
             inverse = Simile(antonym, simile.vehicle)
-            inverse.initialise()
             inverses.append(inverse)
         return inverses
 
@@ -18,14 +19,24 @@ def get_inverses(simile):
 class Simile:
 
     def __init__(self, ground, vehicle, frequency=0, about_frequency=0):
+        global database
 
-        self.ground = wd.get_ground(ground)
-        self.vehicle = wd.get_vehicle(vehicle)
+        if ground not in database.grounds:
+            database.add_ground(ground)
+        if vehicle not in database.vehicles:
+            database.add_vehicle(vehicle)
+
+        self.ground = database.get_ground(ground)
+        self.vehicle = database.get_vehicle(vehicle)
+
         self.frequency = int(frequency)
         self.about_frequency = int(about_frequency)
         self.inverses = []
 
         self.name = "as " + self.ground + " as " + "a|an " + self.vehicle
+
+        #if self.frequency == 0:
+         #   self.determine_frequencies()
 
     @classmethod
     def from_phrase(cls, phrase):
@@ -51,9 +62,6 @@ class Simile:
 
     def initialise(self):
 
-        if self.frequency == 0:
-            self.determine_frequencies()
-
         self.inverses = get_inverses(self)
 
     def determine_frequencies(self):
@@ -65,6 +73,24 @@ class Simile:
 
     def get_frequencies(self):
         return str(self.frequency) + " " + str(self.about_frequency) + "\n"
+
+
+def parse_all():
+
+    global database
+
+    similes = []
+    with open('../res/similesNoDups.txt', 'r') as f:
+        for line in f:
+            #line = line.split(";")
+            words = line.split(" ")
+            ground, vehicle = words[1], words[4]
+            #frequencies = line[1].split(" ")
+
+            simile = Simile(ground, vehicle)
+            simile.initialise()
+            similes.append(simile)
+    database.save()
 
 
 def parse_old():
